ee8b2cd173158cc0a162f4f60abb812a6cab3e94
ta2edchimp <kontakt@andreaswindt.de>
2016-03-30 22:45:56 +0200

    feat(hook): Use applying git config to take submodules and worktrees into account
    Closes #66 & closes #46

--- test/install.test.js
+++ test/install.test.js
@@ -79,6 +79,27 @@ describe('install', function describeInstall() {
 
 })
 
+describe('install using worktree / as a submodule', function describeInstall() {
+  const install = require('../lib/install')
+
+  it('warns when no gitdir specified for worktree / submodule', sinon.test(function test() {
+    fsStub({'.git': ''})
+    const warn = this.stub(console, 'warn')
+    install()
+    expect(warn).to.have.been.calledWithMatch(/this does not seem to be a git project/i)
+  }))
+
+  it('creates hooks directory using gitdir', () => {
+    fsStub({
+      '.git': 'gitdir: ../../a/path/somewhere/else',
+      '../../a/path/somewhere/else': {},
+    })
+    install()
+    expect(fs.existsSync('../../a/path/somewhere/else/hooks')).to.be.true
+  })
+
+})
+
 function fileMode(file) {
   const allOn = 4095 // == 07777 (octal)
   return (fs.statSync(file).mode & allOn) // eslint-disable-line no-bitwise
